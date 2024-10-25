c60821ce9556a214349156d9a4b695bf306c55cd
nikitakhodarev <nikitoshodarev2004@gmail.com>
2024-10-26 02:35:00 +0600

    Update and rename 5_5.cpp to 5.cpp

--- Chapter 5/5.cpp
+++ Chapter 5/5.cpp
@@ -9,21 +9,19 @@ void swapValues(T& a, T& b) {
 }
 
 int main() {
-    // Пример с типом int
+   
     int a = 5;
     int b = 10;
     std::cout << "До обмена (int): a = " << a << ", b = " << b << std::endl;
     swapValues(a, b);
     std::cout << "После обмена (int): a = " << a << ", b = " << b << std::endl;
 
-    // Пример с типом double
     double c = 3.14;
     double d = 2.71;
     std::cout << "До обмена (double): c = " << c << ", d = " << d << std::endl;
     swapValues(c, d);
     std::cout << "После обмена (double): c = " << c << ", d = " << d << std::endl;
 
-    // Пример с типом std::string
     std::string e = "Hello";
     std::string f = "World";
     std::cout << "До обмена (std::string): e = " << e << ", f = " << f << std::endl;
